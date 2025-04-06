cdeaf9e7c6495c48e578216998c9b8ce71a8115e
Josh <joshhuang9508@gmail.com>
2025-04-06 21:46:35 +0800

    refactor: 移除不必要的註解，優化麥克風按鈕樣式，調整上下文選單樣式

--- src/components/viewers/Channel.tsx
+++ src/components/viewers/Channel.tsx
@@ -717,20 +717,6 @@ const ChannelViewer: React.FC<ChannelViewerProps> = React.memo(
     const canApplyMember = memberPermission < 2;
     const canOpenServerSettings = memberPermission > 4;
 
-    // Handlers
-    // FIXME: logic is wrong
-    // const handleAddFavoriteServer = (serverId: Server['id']) => {
-    //   if (!socket) return;
-    //   socket.send.updateUser({
-    //     userId,
-    //     user: {
-    //       ...user,
-    //       favoriteServerId: serverId,
-    //     },
-    //   });
-    //   setIsFavorite(!isFavorite);
-    // };
-
     const handleCreateRootChannel = () => {
       ipcService.popup.open(PopupType.CREATE_CHANNEL);
       ipcService.initialData.onRequest(PopupType.CREATE_CHANNEL, {
@@ -873,10 +859,10 @@ const ChannelViewer: React.FC<ChannelViewerProps> = React.memo(
                       icon: 'locateme',
                       onClick: () => handleLocateUser(),
                     },
-                    {
-                      id: 'separator',
-                      label: '',
-                    },
+                    // {
+                    //   id: 'separator',
+                    //   label: '',
+                    // },
                     // {
                     //   id: 'report',
                     //   label: '舉報',
