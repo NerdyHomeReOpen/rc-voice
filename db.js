481ff57596064e9585c5b1a59c97c4fcb96e8094
Whydog <joshhuang9508@gmail.com>
2025-04-10 11:37:33 +0800

    fix: 移除 db.js 中的註解

--- db.js
+++ db.js
@@ -690,13 +690,6 @@ const Database = {
     },
 
     searchUser: async (querys) => {
-      // const users = (await db.get('users')) || {};
-      // const accountUserIds = (await db.get('accountUserIds')) || {};
-      // const target = Object.values(users).find(
-      //   (u) => u.id === accountUserIds[query],
-      // );
-      // if (!target) return null;
-      // return target;
       try {
         const res = await query(
           `SELECT user_id 
@@ -722,20 +715,6 @@ const Database = {
     },
 
     user: async (userId) => {
-      // const users = (await db.get('users')) || {};
-      // const user = users[userId];
-      // if (!user) return null;
-      // return {
-      //   ...user,
-      //   badges: await get.userBadges(userId),
-      //   friends: await get.userFriends(userId),
-      //   friendGroups: await get.userFriendGroups(userId),
-      //   friendApplications: await get.userFriendApplications(userId),
-      //   joinedServers: await get.userJoinedServers(userId),
-      //   recentServers: await get.userRecentServers(userId),
-      //   ownedServers: await get.userOwnedServers(userId),
-      //   favServers: await get.userFavServers(userId),
-      // };
       try {
         const res = await query(
           `SELECT *
@@ -761,11 +740,6 @@ const Database = {
     },
 
     userFriendGroups: async (userId) => {
-      // const friendGroups = (await db.get('friendGroups')) || {};
-      // return Object.values(friendGroups)
-      //   .filter((fg) => fg.userId === userId)
-      //   .sort((a, b) => b.order - a.order)
-      //   .filter((fg) => fg);
       try {
         const friendGroups = await query(
           `SELECT * 
@@ -791,13 +765,6 @@ const Database = {
     },
 
     userBadges: async (userId) => {
-      // const userBadges = (await db.get('userBadges')) || {};
-      // const badges = (await db.get('badges')) || {};
-      // return Object.values(userBadges)
-      //   .filter((ub) => ub.userId === userId)
-      //   .map((ub) => badges[ub.badgeId])
-      //   .sort((a, b) => b.order - a.order)
-      //   .filter((b) => b);
       try {
         const userBadges = await query(
           `SELECT * 
@@ -825,16 +792,6 @@ const Database = {
     },
 
     userServers: async (userId) => {
-      // const userServers = (await db.get('userServers')) || {};
-      // const servers = (await db.get('servers')) || {};
-      // return Object.values(userServers)
-      //   .filter((us) => us.userId === userId)
-      //   .map((us) => {
-      //     // Concat server data with user server data
-      //     const server = servers[us.serverId];
-      //     return { ...us, ...server };
-      //   })
-      //   .filter((s) => s);
       try {
         const userServers = await query(
           `SELECT *
@@ -862,16 +819,6 @@ const Database = {
     },
 
     userMembers: async (userId) => {
-      // const members = (await db.get('members')) || {};
-      // const servers = (await db.get('servers')) || {};
-      // return Object.values(members)
-      //   .filter((mb) => mb.userId === userId)
-      //   .map((mb) => {
-      //     // Concat member data with server data
-      //     const server = servers[mb.serverId];
-      //     return { ...server, ...mb };
-      //   })
-      //   .filter((mb) => mb);
       try {
         const userMembers = await query(
           `SELECT * 
@@ -899,16 +846,6 @@ const Database = {
     },
 
     userFriends: async (userId) => {
-      // const friends = (await db.get('friends')) || {};
-      // const users = (await db.get('users')) || {};
-      // return Object.values(friends)
-      //   .filter((fd) => fd.userId === userId)
-      //   .map((fd) => {
-      //     // Concat user data with friend data
-      //     const user = users[fd.targetId];
-      //     return { ...user, ...fd };
-      //   })
-      //   .filter((fd) => fd);
       try {
         const userFriends = await query(
           `SELECT * 
@@ -936,19 +873,6 @@ const Database = {
     },
 
     userFriendApplications: async (userId) => {
-      // const applications = (await db.get('friendApplications')) || {};
-      // const users = (await db.get('users')) || {};
-      // return Object.values(applications)
-      //   .filter(
-      //     (app) =>
-      //       app.receiverId === userId && app.applicationStatus === 'pending',
-      //   )
-      //   .map((app) => {
-      //     // Concat user data with friend application data
-      //     const user = users[app.senderId];
-      //     return { ...user, ...app };
-      //   })
-      //   .filter((app) => app);
       try {
         const userFriendApplications = await query(
           `SELECT * 
@@ -1003,16 +927,6 @@ const Database = {
     },
 
     server: async (serverId) => {
-      // const servers = (await db.get('servers')) || {};
-      // const server = servers[serverId];
-      // if (!server) return null;
-      // return {
-      //   ...server,
-      //   channels: await get.serverChannels(serverId),
-      //   members: await get.serverMembers(serverId),
-      //   users: await get.serverUsers(serverId),
-      //   memberApplications: await get.serverMemberApplications(serverId),
-      // };
       try {
         const server = await query(
           `SELECT * 
@@ -1037,17 +951,6 @@ const Database = {
     },
 
     serverUsers: async (serverId) => {
-      // const members = (await db.get('members')) || {};
-      // const users = (await db.get('users')) || {};
-      // return Object.values(members)
-      //   .filter((mb) => mb.serverId === serverId)
-      //   .map((mb) => {
-      //     // Concat user data with member data
-      //     const user = users[mb.userId];
-      //     return { ...user, ...mb };
-      //   })
-      //   .filter((mb) => mb.currentServerId === serverId)
-      //   .filter((mb) => mb);
       try {
         const serverUsers = await query(
           `SELECT * 
@@ -1075,12 +978,6 @@ const Database = {
     },
 
     serverChannels: async (serverId) => {
-      // const channels = (await db.get('channels')) || {};
-      // const categories = (await db.get('categories')) || {};
-      // return Object.values({ ...channels, ...categories })
-      //   .filter((ch) => ch.serverId === serverId)
-      //   .sort((a, b) => a.order - b.order)
-      //   .filter((ch) => ch);
       try {
         const serverChannels = await query(
           `SELECT * 
@@ -1106,16 +1003,6 @@ const Database = {
     },
 
     serverMembers: async (serverId) => {
-      // const members = (await db.get('members')) || {};
-      // const users = (await db.get('users')) || {};
-      // return Object.values(members)
-      //   .filter((mb) => mb.serverId === serverId)
-      //   .map((mb) => {
-      //     // Concat user data with member data
-      //     const user = users[mb.userId];
-      //     return { ...user, ...mb };
-      //   })
-      //   .filter((mb) => mb);
       try {
         const serverMembers = await query(
           `SELECT * 
@@ -1143,19 +1030,6 @@ const Database = {
     },
 
     serverMemberApplications: async (serverId) => {
-      // const applications = (await db.get('memberApplications')) || {};
-      // const users = (await db.get('users')) || {};
-      // return Object.values(applications)
-      //   .filter(
-      //     (app) =>
-      //       app.serverId === serverId && app.applicationStatus === 'pending',
-      //   )
-      //   .map((app) => {
-      //     // Concat user data with application data
-      //     const user = users[app.userId];
-      //     return { ...user, ...app };
-      //   })
-      //   .filter((app) => app);
       try {
         const serverMemberApplications = await query(
           `SELECT * 
@@ -1184,12 +1058,6 @@ const Database = {
     },
 
     category: async (categoryId) => {
-      // const categories = (await db.get('categories')) || {};
-      // const category = categories[categoryId];
-      // if (!category) return null;
-      // return {
-      //   ...category,
-      // };
       try {
         const category = await query(
           `SELECT * 
@@ -1215,16 +1083,6 @@ const Database = {
     },
 
     channel: async (channelId) => {
-      // const channels = (await db.get('channels')) || {};
-      // const channel = channels[channelId];
-      // if (!channel) return null;
-      // return {
-      //   ...channel,
-      //   messages: [
-      //     ...(await get.channelMessages(channelId)),
-      //     ...(await get.channelInfoMessages(channelId)),
-      //   ],
-      // };
       try {
         const channel = await query(
           `SELECT * 
@@ -1250,18 +1108,6 @@ const Database = {
     },
 
     channelMessages: async (channelId) => {
-      // const messages = (await db.get('messages')) || {};
-      // const members = (await db.get('members')) || {};
-      // const users = (await db.get('users')) || {};
-      // return Object.values(messages)
-      //   .filter((msg) => msg.channelId === channelId && msg.type === 'general')
-      //   .map((msg) => {
-      //     // Concat user and member data with message data
-      //     const member = members[`mb_${msg.senderId}-${msg.serverId}`];
-      //     const user = users[msg.senderId];
-      //     return { ...user, ...member, ...msg };
-      //   })
-      //   .filter((msg) => msg);
       try {
         const channelMessages = await query(
           `SELECT * 
@@ -1288,13 +1134,6 @@ const Database = {
     },
 
     channelInfoMessages: async (channelId) => {
-      // const messages = (await db.get('messages')) || {};
-      // return Object.values(messages)
-      //   .filter((msg) => msg.channelId === channelId && msg.type === 'info')
-      //   .map((msg) => {
-      //     return { ...msg };
-      //   })
-      //   .filter((msg) => msg);
       try {
         const channelInfoMessages = await query(
           `SELECT * 
@@ -1321,12 +1160,6 @@ const Database = {
     },
 
     friendGroup: async (friendGroupId) => {
-      // const friendGroups = (await db.get('friendGroups')) || {};
-      // const friendGroup = friendGroups[friendGroupId];
-      // if (!friendGroup) return null;
-      // return {
-      //   ...friendGroup,
-      // };
       try {
         const friendGroup = await query(
           `SELECT * 
@@ -1352,12 +1185,6 @@ const Database = {
     },
 
     member: async (userId, serverId) => {
-      // const members = (await db.get('members')) || {};
-      // const member = members[`mb_${userId}-${serverId}`];
-      // if (!member) return null;
-      // return {
-      //   ...member,
-      // };
       try {
         const member = await query(
           `SELECT * 
@@ -1384,12 +1211,6 @@ const Database = {
     },
 
     memberApplication: async (userId, serverId) => {
-      // const applications = (await db.get('memberApplications')) || {};
-      // const application = applications[`ma_${userId}-${serverId}`];
-      // if (!application) return null;
-      // return {
-      //   ...application,
-      // };
       try {
         const memberApplication = await query(
           `SELECT * 
@@ -1416,12 +1237,6 @@ const Database = {
     },
 
     friend: async (userId, targetId) => {
-      // const friends = (await db.get('friends')) || {};
-      // const friend = friends[`fd_${userId}-${targetId}`];
-      // if (!friend) return null;
-      // return {
-      //   ...friend,
-      // };
       try {
         const friend = await query(
           `SELECT * 
@@ -1448,12 +1263,6 @@ const Database = {
     },
 
     friendApplication: async (senderId, receiverId) => {
-      // const applications = (await db.get('friendApplications')) || {};
-      // const application = applications[`fa_${senderId}-${receiverId}`];
-      // if (!application) return null;
-      // return {
-      //   ...application,
-      // };
       try {
         const friendApplication = await query(
           `SELECT * 
@@ -1480,12 +1289,6 @@ const Database = {
     },
 
     message: async (messageId) => {
-      // const messages = (await db.get('messages')) || {};
-      // const message = messages[messageId];
-      // if (!message) return null;
-      // return {
-      //   ...message,
-      // };
       try {
         const message = await query(
           `SELECT * 
@@ -1511,17 +1314,6 @@ const Database = {
     },
 
     directMessages: async (userId, targetId) => {
-      // const directMessages = (await db.get('directMessages')) || {};
-      // const users = (await db.get('users')) || {};
-      // const userId1 = userId.localeCompare(targetId) < 0 ? userId : targetId;
-      // const userId2 = userId.localeCompare(targetId) < 0 ? targetId : userId;
-      // return Object.values(directMessages)
-      //   .filter((dm) => dm.userId1 === userId1 && dm.userId2 === userId2)
-      //   .map((dm) => {
-      //     const user = users[dm.senderId];
-      //     return { ...user, ...dm };
-      //   })
-      //   .filter((dm) => dm);
       try {
         const userId1 = userId.localeCompare(targetId) < 0 ? userId : targetId;
         const userId2 = userId.localeCompare(targetId) < 0 ? targetId : userId;
